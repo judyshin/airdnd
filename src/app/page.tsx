3ef4c858350cef23b4b310822748794cf242c932
kiwisae99 <judyshin99@gmail.com>
2024-06-14 18:36:24 +0900

    Airdnd 배포

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,41 @@
-// import Link from 'next/link'
-// import Category from '@/components/accommodation/categoryBundle/Category'
-// import getRoomsList from '@/app/apis/fetchMainPage/getRoomsList'
-// import Card from '@/components/card/card'
-// import InfiniteScroll from '@/components/infiniteScroll/scroll'
+import Link from 'next/link'
+import Category from '@/components/accommodation/categoryBundle/Category'
+import getRoomsList from '@/app/apis/fetchMainPage/getRoomsList'
+import Card from '@/components/card/card'
+import InfiniteScroll from '@/components/infiniteScroll/scroll'
 
-// function RoomsItem({ accommodation }) {
-//   return (
-//     <Link href={`/rooms/${accommodation.accommodationId}`}>
-//       <Card
-//         accommodationName={accommodation.accommodationName}
-//         imageUrl={accommodation.imageUrl}
-//         pricePerDay={accommodation.pricePerDay}
-//         rating={accommodation.rating}
-//         guestFavorite={accommodation.guestFavorite}
-//       />
-//     </Link>
-//   )
-// }
+function RoomsItem({ accommodation }) {
+  return (
+    <Link href={`/rooms/${accommodation.accommodationId}`}>
+      <Card
+        accommodationName={accommodation.accommodationName}
+        imageUrl={accommodation.imageUrl}
+        pricePerDay={accommodation.pricePerDay}
+        rating={accommodation.rating}
+        guestFavorite={accommodation.guestFavorite}
+      />
+    </Link>
+  )
+}
 
-export default function Home() {
+export default async function Home() {
   // 설정한 id부터 40개의 숙소 데이터를 가져옵니다.
-  // const id = 1
+  const id = 1
   // 리스트에 어떤 항목을 포함할지 배열로 전달합니다.
-  // const fields = [
-  //   'accommodationId',
-  //   'accommodationName',
-  //   'imageUrl',
-  //   'guestFavorite',
-  //   'rating',
-  //   'pricePerDay',
-  // ]
+  const fields = [
+    'accommodationId',
+    'accommodationName',
+    'imageUrl',
+    'guestFavorite',
+    'rating',
+    'pricePerDay',
+  ]
   // 설정된 정보를 조회해서 배열로 반환된 정보를 할당합니다.
-  // const accommodations = await getRoomsList(id, fields)
+  const accommodations = await getRoomsList(id, fields)
 
   return (
     <div className='flex flex-col items-center justify-start'>
-      <h1>Main Page</h1>
-      {/* <div className='w-full max-w-[1760px] grow items-center justify-between'>
+      <div className='w-full max-w-[1760px] grow items-center justify-between'>
         <Category />
       </div>
       <div className='mt-3 grid w-full max-w-[1760px] grid-cols-1 gap-4  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
@@ -44,7 +43,7 @@ export default function Home() {
           <RoomsItem key={`RoomsItem-${index}`} accommodation={accommodation} />
         ))}
         <InfiniteScroll></InfiniteScroll>
-      </div> */}
+      </div>
     </div>
   )
 }
--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -1,20 +1,19 @@
-// import DetailPageBanner from '@/components/accommodation/detailBanner/DetailPageBanner'
-// import HostDetail from '@/components/accommodation/hostDetail/HostDetailContainer'
-// import InfoWrapper from '@/components/accommodation/infoWrapper/InfoWrapper'
-// import AmenityInfo from '@/components/accommodation/amenityInfo/AmenityInfo'
-// import DetailCalendar from '@/components/accommodation/detailCalendar'
+import DetailPageBanner from '@/components/accommodation/detailBanner/DetailPageBanner'
+import HostDetail from '@/components/accommodation/hostDetail/HostDetailContainer'
+import InfoWrapper from '@/components/accommodation/infoWrapper/InfoWrapper'
+import AmenityInfo from '@/components/accommodation/amenityInfo/AmenityInfo'
+import DetailCalendar from '@/components/accommodation/detailCalendar'
 
 function AccommodationPage({ params }) {
   const { id } = params
 
   return (
     <>
-      {/* <DetailPageBanner id={id} />
+      <DetailPageBanner id={id} />
       <InfoWrapper id={id} />
       <AmenityInfo id={id} />
       <DetailCalendar />
-      <HostDetail id={id} /> */}
-      <div>Room {id}</div>
+      <HostDetail id={id} />
     </>
   )
 }
